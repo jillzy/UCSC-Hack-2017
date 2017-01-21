46f452bb8a30314bd17c487bec180420c5397231
jillzy <jillzyeung@gmail.com>
2017-01-21 15:00:44 -0800

    before merge w wesly branch

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8f40fa7646e68ccc$00abeb825beab6cc834450d38aa7c9469e5a6d6f"
+password="pbkdf2(1000,20,sha512)$b826f1099e6e0db2$cee6062e2bff89592f6eac13ad2810bac500b02d"
