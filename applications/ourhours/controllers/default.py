763d450fecc79101dd31de366505059b4bb2199f
weslylim94 <welim@ucsc.edu>
2017-01-22 01:19:18 -0800

    revised version

--- applications/ourhours/controllers/default.py
+++ applications/ourhours/controllers/default.py
@@ -3,32 +3,43 @@ import imaplib
 import email
 from email.header import decode_header
 
+mail = Mail()
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'ourhours1337@gmail.com'
+mail.settings.login = 'ourhours1337@gmail.com:weslysmellsgood'
+
 import random
 import string
+
+
 def index():
     groups = db(db.groups).select(db.groups.ALL)
     return locals()
 
-# @auth.requires_login()
+
 def create():
+    pw = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(5)])
+    db.groups.group_code.default = pw
     form = SQLFORM(db.groups, deletable=True).process()
     if form.accepted:
-        
+
+        mail.send(to=[request.vars.email],
+            subject="Password for " + request.vars.group_name,
+            message= pw)
         redirect(URL('index'))
     return locals()
 
 def join():
-    groups = db(db.groups).select(db.groups.group_code)
+    groups = db(db.groups).select(db.groups.ALL)
     form = SQLFORM(db.join_group, deletable=True).process()
     if form.accepted:
         for groups in groups:
-            # response.flash = groups.group_code
             if request.vars.join_code in groups.group_code:
-                response.flash = 'Welcome to the group!'
-                redirect(URL('index'))
+                redirect(URL('event_page', args=groups.id))
+            else:
+                response.flash = "Incorrect join code"
     return locals()
 
-# @auth.requires_login()
 def ecreate():
     form = SQLFORM(db.event, deletable=True).process()
     if form.accepted:
@@ -38,17 +49,14 @@ def ecreate():
 import json
 from datetime import datetime
 from json import dumps
-# @auth.requires_login()
+
 def event_page():
     #currently faulty, returns all events
     g = db.groups(request.args(0,cast=int)) or redirect(URL('index'))
     db.event.event_id.default = g.id
     db.event.event_code.default = g.group_code
-    # event = db(db.event.event_id == g.id).select()
     event = db(db.event).select(db.event.ALL)
     json_data = json.dumps([[e.event_allDay, e.event_name, e.event_id, e.start.strftime("%B %d, %Y"), e.end.strftime("%B %d, %Y"), e.event_description] for e in event])
-    response.flash = json_data
-
     return locals()
 
 def user():
@@ -59,8 +67,3 @@ def download():
 
 def picture():
     return response.view(request, db)
-
-def getdata():
-    rows = db().select(db.realtimedata.id, db.realtimedata.FlowRate,
-                       limitby=(0, 3), orderby=~db.realtimedata.id)
-    return json.dumps([[r.id, r.FlowRate] for r in rows])
--- applications/ourhours/models/markmin.py
+++ applications/ourhours/models/markmin.py
@@ -8,9 +8,10 @@ import random
 import string
 
 db.define_table('groups',
+                Field('email', requires = IS_EMAIL(error_message='invalid email!')),
                 Field('group_name', 'string'),
                 Field('optional_group_photo', 'upload'),
-                Field('group_code', 'string', default=''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(5)])),
+                Field('group_code', 'string'),
                 Field('group_description', 'text', requires=IS_LENGTH(maxsize=80), default='No Group Description.'))
 
 db.define_table('event',
@@ -29,44 +30,3 @@ db.define_table('join_group',
 db.groups.group_code.writable = db.groups.group_code.readable = False
 db.event.event_id.writable = db.event.event_id.readable = False
 db.event.event_code.writable = db.event.event_code.readable = False
-# import gluon.template
-
-# markmin_dict = dict(
-#     code_python=lambda code: str(CODE(code)),
-#     template=lambda
-#     code: gluon.template.render(code, context=globals()),
-#     sup=lambda
-#     code: '<sup style="font-size:0.5em;">%s</sup>' % code,
-#     br=lambda n: '<br>' * int(n),
-#     groupdates=lambda group: group_feed_reader(group),
-# )
-
-
-# def get_content(b=None,
-#                 c=request.controller,
-#                 f=request.function,
-#                 l='en',
-#                 format='markmin'):
-#     """Gets and renders the file in
-#     <app>/private/content/<lang>/<controller>/<function>/<block>.<format>
-#     """
-
-#     def openfile():
-#         import os
-#         path = os.path.join(
-#             request.folder, 'private', 'content', l, c, f, b + '.' + format)
-#         return open(path)
-
-#     try:
-#         openedfile = openfile()
-#     except Exception, IOError:
-#         l = 'en'
-#         openedfile = openfile()
-
-#     if format == 'markmin':
-#         html = MARKMIN(str(T(openedfile.read())), markmin_dict)
-#     else:
-#         html = str(T(openedfile.read()))
-#     openedfile.close()
-
-#     return html
